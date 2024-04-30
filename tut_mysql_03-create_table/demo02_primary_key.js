752d7eecc39088a9f0714a0bb54224b368e0cbb7
Kells_P <kellsping@gmail.com>
2024-04-30 14:08:22 +0800

    Learning "MySQL Create Table" complete.

---
+++ tut_mysql_03-create_table/demo02_primary_key.js
@@ -0,0 +1,18 @@
+import mysql from "mysql";
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "yourusername",
+  password: "yourpassword",
+  database: "yourdb"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  var sql = "ALTER TABLE customers ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Table altered");
+  });
+});
