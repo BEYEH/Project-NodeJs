ae60de0a0698ce2945e087ee0e437b707515c26c
Kells_P <kellsping@gmail.com>
2024-04-30 15:48:41 +0800

    Learning "MySQL Order By" complete.

---
+++ tut_mysql_07-order_by/demo02-sort_result_desc.js
@@ -0,0 +1,23 @@
+import mysql from "mysql";
+
+var host = "localhost";
+var usr = "yourusername";
+var pwd = "yourpassword";
+var db = "yourdb";
+
+var sql = "SELECT * FROM customers ORDER BY name DESC";
+
+var con = mysql.createConnection({
+  host: host,
+  user: usr,
+  password: pwd,
+  database: db,
+});
+
+con.connect(function (err) {
+  if (err) throw err;
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
