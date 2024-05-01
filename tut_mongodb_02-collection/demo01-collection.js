f10cc759b8db21425df56b2c34245f711b611870
kells\KellsPing <kellsping@gmail.com>
2024-05-01 15:21:10 +0800

    Learning "MongoDB Collection" complete.

---
+++ tut_mongodb_02-collection/demo01-collection.js
@@ -0,0 +1,76 @@
+import mongoose from "mongoose";
+
+const serverUri = "mongodb://localhost:27017/";
+const dbName = "mydb";
+const collectionName = "customers";
+
+const connectToServer = async () => {
+  console.log("connectToServer...");
+  await mongoose
+    .connect(serverUri)
+    .then(() => {
+      console.log("Connected to MongoDB server.");
+    })
+    .catch((err) => {
+      console.error("Error connecting to MongoDB server:", err);
+    });
+};
+
+const connectToDatabase = () => {
+  console.log("connectToDatabase...");
+  try {
+    const db = mongoose.connection.useDb(dbName);
+    console.log(`Switched to database '${db.name}'`);
+  } catch (err) {
+    console.error("Error switching to database:", err);
+  }
+};
+
+const createColl = async () => {
+  console.log("createColl...");
+  if (mongoose.connection.readyState !== 1) {
+    throw new Error("MongoDB connection not ready");
+  }
+
+  const customerSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    age: Number,
+  });
+
+  const Customer = mongoose.model(collectionName, customerSchema);
+
+  try {
+    await Customer.createCollection();
+    console.log("Collection created!");
+  } catch (err) {
+    console.error("Error creating collection:", err);
+    throw err;
+  }
+};
+
+const disconnectFromServer = () => {
+  console.log("disconnectFromServer...");
+  mongoose
+    .disconnect()
+    .then(() => {
+      console.log("Disconnected from MongoDB server.");
+    })
+    .catch((err) => {
+      console.error("Error disconnecting from MongoDB server:", err);
+      throw err;
+    });
+};
+
+const main = async () => {
+  try {
+    await connectToServer();
+    connectToDatabase();
+    await createColl();
+    disconnectFromServer();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+main();
